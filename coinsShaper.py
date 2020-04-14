fe9c55a05ab6901bb9ceca63251a8855bc049cac
Alexander Kazansky <kazansky85@gmail.com>
2020-04-15 00:27:13 +0300

    add python version

---
+++ coinsShaper.py
@@ -0,0 +1,79 @@
+import numpy as np
+import cv2 as cv
+
+blurSize = 5
+thresh = 180
+threshType = cv.THRESH_OTSU
+minContourSize = 50
+debugSave = True
+
+def preprocess(im):
+   imgray = cv.cvtColor(im, cv.COLOR_BGR2GRAY)
+   imblur = cv.blur(imgray,(blurSize, blurSize))
+   ret, imth = cv.threshold(imblur, thresh, 255, threshType)
+   return imth
+
+def getContoursRects(th):
+    contours, hierarchy = cv.findContours(th, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    rects = list(map(lambda contour: cv.boundingRect(contour), contours))
+    rects = filter(lambda rect: rect[2]>minContourSize and rect[3]>minContourSize, rects)
+    return list(rects)
+
+def rectCompare(r1, r2):
+    s1 = r1[2]*r1[3]
+    s2 = r2[2]*r2[3]
+    if abs(s1/s2 - 1.0) > 0.2:
+        # square ratio is too big, it can't be different side of one object
+        return False
+    x=max(r1[0], r2[0])
+    y=max(r1[1], r2[1])
+    w=min(r1[0]+r1[2], r2[0]+r2[2])
+    h=min(r1[1]+r1[3], r2[1]+r2[3])
+
+    if x>w or y>h:
+        return False
+
+    s=(w-x)*(h-y)
+
+    return s>0
+
+def getCroppedImage(img, rect):
+    ofset = blurSize * 3
+    s = img.shape
+    x1 = max(0, rect[1] - ofset )
+    x2 = min(s[0], rect[1]+rect[3] + ofset)
+    y1 = max(0, rect[0] - ofset )
+    y2 = min(s[1], rect[0]+rect[2] + ofset)
+    return img[x1:x2, y1:y2]
+
+im1 = cv.imread('/Users/aknew/Documents/samples-shape/IMG_2729.jpeg')
+im2 = cv.imread('/Users/aknew/Documents/samples-shape/IMG_2730.jpeg')
+
+th1= preprocess(im1)
+th2= preprocess(im2)
+
+if debugSave:
+    cv.imwrite("th1.jpg", th1)
+    cv.imwrite("th2.jpg", th2)
+
+r1 = getContoursRects(th1)
+r2 = getContoursRects(th2)
+
+number = 0
+for rect1 in r1:
+    for rect2 in r2:
+        if rectCompare(rect1, rect2):
+            cropedim1 = getCroppedImage(im1, rect1)
+            cropedim2 = getCroppedImage(im2, rect2)
+
+            # from https://stackoverflow.com/questions/7589012/combining-two-images-with-opencv
+            h1, w1 = cropedim1.shape[:2]
+            h2, w2 = cropedim2.shape[:2]
+            #create empty matrix
+            vis = np.zeros((max(h1, h2), w1+w2,3), np.uint8)
+
+            #combine 2 images
+            vis[:h1, :w1,:3] = cropedim1
+            vis[:h2, w1:w1+w2,:3] = cropedim2
+            cv.imwrite("img{}.jpg".format(number), vis)
+            number += 1
